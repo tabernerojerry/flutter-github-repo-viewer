9032cf6e24112918204214dff0d4c0ceb8909d23
Jerry Tabernero <jerry.tabernero@opcbiz.com>
2021-07-03 13:35:00 +0800

    sign in

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,11 +1,39 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/shared/providers.dart';
+import 'package:repo_viewer/core/application/auth_notifier.dart';
 import 'package:repo_viewer/core/presentation/routes/app_router.gr.dart';
 
-class AppWidget extends StatelessWidget {
+final initializationProvider = FutureProvider<Unit>((ref) async {
+  final authNotifier = ref.read(authNotifierProvider.notifier);
+  await authNotifier.checkAndUpdateAuthState();
+  return unit;
+});
+
+class AppWidget extends ConsumerWidget {
   final _appRouter = AppRouter();
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(initializationProvider, (_) {});
+    ref.listen<AuthState>(authNotifierProvider, (state) {
+      state.maybeMap(
+          orElse: () {},
+          authenticated: (_) {
+            _appRouter.pushAndPopUntil(
+              const StarredReposRoute(),
+              predicate: (_) => false,
+            );
+          },
+          unauthenticated: (_) {
+            _appRouter.pushAndPopUntil(
+              const SignInRoute(),
+              predicate: (_) => false,
+            );
+          });
+    });
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Repo Viewer',
       routerDelegate: _appRouter.delegate(),
       routeInformationParser: _appRouter.defaultRouteParser(),
--- lib/splash/presentation/splash_page.dart
+++ lib/splash/presentation/splash_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Icon(
+            MdiIcons.github,
+            size: 150.0,
+          ),
+          SizedBox(height: 8.0),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40.0),
+            child: LinearProgressIndicator(),
+          ),
+        ],
+      ),
+    );
   }
 }
