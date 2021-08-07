429af1c04bc463331d1b35e68da8cbe579b5a92e
Jerry Tabernero <jerry.tabernero@opcbiz.com>
2021-08-07 18:11:36 +0800

    get starred repos

--- lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -1,7 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:repo_viewer/core/infrastructure/network_exceptions.dart';
 import 'package:repo_viewer/core/infrastructure/remote_response.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers.dart';
 import 'package:repo_viewer/github/core/infrastructure/github_headers_cache.dart';
 import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
+import 'package:repo_viewer/core/infrastructure/dio_extensions.dart';
 
 class StarredReposRemoteService {
   final Dio _dio;
@@ -13,5 +16,55 @@ class StarredReposRemoteService {
   );
 
   Future<RemoteResponse<List<GithubRepoDto>>> getStarredReposPage(
-      int page) async {}
+    int page,
+  ) async {
+    const token = 'dasdaasds';
+    const accept = 'application/vnd.github.v3.html+json';
+
+    final requestUri = Uri.https(
+      'api.github.com',
+      '/user/starred',
+      {'page': '$page'},
+    );
+
+    final previousHeaders = await _headersCache.getHeaders(requestUri);
+
+    try {
+      final response = await _dio.getUri(
+        requestUri,
+        options: Options(
+          headers: {
+            'Authorization': 'bearer $token',
+            'Accept': accept,
+            'If-None-Match': previousHeaders?.etag ?? '',
+          },
+        ),
+      );
+
+      if (response.statusCode == 304) {
+        return const RemoteResponse.notModified();
+      } else if (response.statusCode == 200) {
+        final headers = GithubHeaders.parse(response);
+        await _headersCache.saveHeaders(requestUri, headers);
+
+        final convertedData = (response.data as List<dynamic>)
+            .map((e) => GithubRepoDto.fromJson(e as Map<String, dynamic>))
+            .toList();
+
+        return RemoteResponse.withNewData(convertedData);
+      } else {
+        /// This else check is never run but just incase,
+        /// something went wrong with the server
+        throw RestApiException(response.statusCode);
+      }
+    } on DioError catch (e) {
+      if (e.isNoConnectionError) {
+        return const RemoteResponse.noConnection();
+      } else if (e.response != null) {
+        throw RestApiException(e.response?.statusCode);
+      } else {
+        rethrow;
+      }
+    }
+  }
 }
