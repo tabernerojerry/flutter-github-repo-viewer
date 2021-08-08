4229669c668adf48d857d7e8ba0c07fa7b1688e3
Jerry Tabernero <jerry.tabernero@opcbiz.com>
2021-08-08 17:03:16 +0800

    starred repos local service getPage

--- lib/github/repos/starred_repos/infrastructure/starred_repos_local_service.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_local_service.dart
@@ -1,5 +1,6 @@
 import 'package:repo_viewer/core/infrastructure/sembast_database.dart';
 import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
+import 'package:repo_viewer/github/core/infrastructure/pagination_config.dart';
 import 'package:sembast/sembast.dart';
 import 'package:collection/collection.dart';
 
@@ -14,11 +15,26 @@ class StarredReposLocalService {
 
     await _store
         .records(
-          dtos.mapIndexed((index, _) => index + 3 * sembastPage),
+          dtos.mapIndexed((index, _) =>
+              index + PaginationConfig.itemsPerPage * sembastPage),
         )
         .put(
           _sembastDatabase.instance,
           dtos.map((e) => e.toJson()).toList(),
         );
+  }
+
+  Future<List<GithubRepoDto>> getPage(int page) async {
+    final sembastPage = page - 1;
+
+    final records = await _store.find(
+      _sembastDatabase.instance,
+      finder: Finder(
+        limit: PaginationConfig.itemsPerPage,
+        offset: PaginationConfig.itemsPerPage * sembastPage,
+      ),
+    );
+
+    return records.map((e) => GithubRepoDto.fromJson(e.value)).toList();
   }
 }
