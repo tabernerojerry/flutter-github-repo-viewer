6fe1435396fc5494e6e8bd3ae53a6c90951693bd
Jerry Tabernero <jerry.tabernero@opcbiz.com>
2021-09-16 17:38:40 +0800

    removed duplicate paginated repos notifier

---
+++ lib/github/repos/core/application/paginated_repos_notifier.dart
@@ -0,0 +1,66 @@
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/core/domain/fresh.dart';
+import 'package:repo_viewer/github/core/domain/github_failure.dart';
+import 'package:repo_viewer/github/core/domain/github_repo.dart';
+import 'package:repo_viewer/github/core/infrastructure/pagination_config.dart';
+
+part 'paginated_repos_notifier.freezed.dart';
+
+typedef RepositoryGetter
+    = Future<Either<GithubFailure, Fresh<List<GithubRepo>>>> Function(int page);
+
+@freezed
+class PaginatedReposState with _$PaginatedReposState {
+  const PaginatedReposState._();
+  const factory PaginatedReposState.initial(
+    Fresh<List<GithubRepo>> repos,
+  ) = _Initial;
+  const factory PaginatedReposState.loadInProgress(
+    Fresh<List<GithubRepo>> repos,
+    int itemsPerPage,
+  ) = _LoadInProgress;
+  const factory PaginatedReposState.loadSuccess(
+    Fresh<List<GithubRepo>> repos, {
+    required bool isNextPageAvailable,
+  }) = _LoadSuccess;
+  const factory PaginatedReposState.loadFailuer(
+      Fresh<List<GithubRepo>> repos, GithubFailure failure) = _LoadFailure;
+}
+
+class PaginatedReposNotifier extends StateNotifier<PaginatedReposState> {
+  int _page = 1;
+
+  PaginatedReposNotifier() : super(PaginatedReposState.initial(Fresh.yes([])));
+
+  Future<void> getNextPage(
+    RepositoryGetter getter,
+  ) async {
+    state = PaginatedReposState.loadInProgress(
+      Fresh.no(state.repos.entity),
+      PaginationConfig.itemsPerPage,
+    );
+
+    final failureOrRepos = await getter(_page);
+
+    state = failureOrRepos.fold(
+      (failure) => PaginatedReposState.loadFailuer(
+        state.repos,
+        failure,
+      ),
+      (data) {
+        _page += 1;
+        return PaginatedReposState.loadSuccess(
+          data.copyWith(
+            entity: [
+              ...state.repos.entity,
+              ...data.entity,
+            ],
+          ),
+          isNextPageAvailable: data.isNextPageAvailable ?? false,
+        );
+      },
+    );
+  }
+}
--- lib/github/repos/starred_repos/application/starred_repos_notifier.dart
+++ lib/github/repos/starred_repos/application/starred_repos_notifier.dart
@@ -1,64 +1,12 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:repo_viewer/core/domain/fresh.dart';
-import 'package:repo_viewer/github/core/domain/github_failure.dart';
-import 'package:repo_viewer/github/core/domain/github_repo.dart';
-import 'package:repo_viewer/github/core/infrastructure/pagination_config.dart';
+import 'package:repo_viewer/github/repos/core/application/paginated_repos_notifier.dart';
 import 'package:repo_viewer/github/repos/starred_repos/infrastructure/starred_repos_repository.dart';
 
-part 'starred_repos_notifier.freezed.dart';
-
-@freezed
-class StarredReposState with _$StarredReposState {
-  const StarredReposState._();
-  const factory StarredReposState.initial(
-    Fresh<List<GithubRepo>> repos,
-  ) = _Initial;
-  const factory StarredReposState.loadInProgress(
-    Fresh<List<GithubRepo>> repos,
-    int itemsPerPage,
-  ) = _LoadInProgress;
-  const factory StarredReposState.loadSuccess(
-    Fresh<List<GithubRepo>> repos, {
-    required bool isNextPageAvailable,
-  }) = _LoadSuccess;
-  const factory StarredReposState.loadFailuer(
-      Fresh<List<GithubRepo>> repos, GithubFailure failure) = _LoadFailure;
-}
-
-class StarredReposNotifier extends StateNotifier<StarredReposState> {
+class StarredReposNotifier extends PaginatedReposNotifier {
   final StarredReposRepository _repsitory;
 
-  int _page = 1;
-
-  StarredReposNotifier(this._repsitory)
-      : super(StarredReposState.initial(Fresh.yes([])));
+  StarredReposNotifier(this._repsitory);
 
   Future<void> getNextStarredReposPage() async {
-    state = StarredReposState.loadInProgress(
-      Fresh.no(state.repos.entity),
-      PaginationConfig.itemsPerPage,
-    );
-
-    final failureOrRepos = await _repsitory.getStarredReposPage(_page);
-
-    state = failureOrRepos.fold(
-      (failure) => StarredReposState.loadFailuer(
-        state.repos,
-        failure,
-      ),
-      (data) {
-        _page += 1;
-        return StarredReposState.loadSuccess(
-          data.copyWith(
-            entity: [
-              ...state.repos.entity,
-              ...data.entity,
-            ],
-          ),
-          isNextPageAvailable: data.isNextPageAvailable ?? false,
-        );
-      },
-    );
+    super.getNextPage((page) => _repsitory.getStarredReposPage(page));
   }
 }
