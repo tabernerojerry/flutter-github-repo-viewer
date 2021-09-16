0f9a53f656493a630fb569f72a73d38ac7b0690f
Jerry Tabernero <jerry.tabernero@opcbiz.com>
2021-09-16 16:55:47 +0800

    removed duplicate repos remote service

--- lib/github/repos/searched_repos/infrastructure/searched_repos_remote_service.dart
+++ lib/github/repos/searched_repos/infrastructure/searched_repos_remote_service.dart
@@ -5,24 +5,26 @@ import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
 import 'package:repo_viewer/github/core/infrastructure/pagination_config.dart';
 import 'package:repo_viewer/github/repos/core/infrastructure/repos_remote_service.dart';
 
-class StarredReposRemoteService extends ReposRemoteService {
-  StarredReposRemoteService(
+class SearchedReposRemoteService extends ReposRemoteService {
+  SearchedReposRemoteService(
     Dio dio,
     GithubHeadersCache headersCache,
   ) : super(dio, headersCache);
 
-  Future<RemoteResponse<List<GithubRepoDto>>> getStarredReposPage(
+  Future<RemoteResponse<List<GithubRepoDto>>> getSearchedReposPage(
+    String query,
     int page,
   ) async =>
       super.getPage(
         requestUri: Uri.https(
           'api.github.com',
-          '/user/starred',
+          '/search/repositories',
           {
+            'q': query,
             'page': '$page',
             'per_page': PaginationConfig.itemsPerPage.toString(),
           },
         ),
-        jsonDataSelector: (json) => json as List<dynamic>,
+        jsonDataSelector: (json) => json['items'] as List<dynamic>,
       );
 }
