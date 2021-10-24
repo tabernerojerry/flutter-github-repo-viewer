e9688da099ed469cabcce8f49ddb0c88d7b17ccf
Jerry Tabernero <jerry.tabernero@opcbiz.com>
2021-10-24 14:18:26 +0800

    minimum viable search bar

--- lib/search/presentation/search_bar.dart
+++ lib/search/presentation/search_bar.dart
@@ -69,7 +69,30 @@ class _SearchBarState extends ConsumerState<SearchBar> {
         )
       ],
       builder: (context, transition) {
-        return Container();
+        return Consumer(
+          builder: (context, ref, child) {
+            final searchHistoryState = ref.watch(searchHistoryNotifierProvider);
+            return searchHistoryState.map(
+              data: (history) {
+                return Column(
+                  children: history.value
+                      .map(
+                        (term) => ListTile(
+                          title: Text(term),
+                        ),
+                      )
+                      .toList(),
+                );
+              },
+              loading: (_) => const ListTile(
+                title: LinearProgressIndicator(),
+              ),
+              error: (_) => ListTile(
+                title: Text('Very unexpected error ${_.error}'),
+              ),
+            );
+          },
+        );
       },
     );
   }
