2ec3cdd00836072720ba61485a96033dcdac146e
Jerry Tabernero <jerry.tabernero@opcbiz.com>
2021-10-24 18:45:09 +0800

    repo detail remote service

---
+++ lib/github/detail/infrastructure/repo_detail_remote_service.dart
@@ -0,0 +1,60 @@
+import 'package:dio/dio.dart';
+import 'package:repo_viewer/core/infrastructure/network_exceptions.dart';
+import 'package:repo_viewer/core/infrastructure/remote_response.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers_cache.dart';
+import 'package:repo_viewer/core/infrastructure/dio_extensions.dart';
+
+class RepoDetailRemoteService {
+  final Dio _dio;
+  final GithubHeadersCache _headersCache;
+
+  RepoDetailRemoteService(
+    this._dio,
+    this._headersCache,
+  );
+
+  Future<RemoteResponse<String>> getReadMeHtml(String fullRepoName) async {
+    final requestUri = Uri.https(
+      'api.github.com',
+      '/',
+    );
+
+    final previousHeaders = await _headersCache.getHeaders(requestUri);
+
+    try {
+      final response = await _dio.getUri(
+        requestUri,
+        options: Options(
+          headers: {
+            'If-None-Match': previousHeaders?.etag ?? '',
+          },
+          responseType: ResponseType.plain,
+        ),
+      );
+
+      if (response.statusCode == 304) {
+        return const RemoteResponse.notModified(maxPage: 0);
+      } else if (response.statusCode == 200) {
+        final headers = GithubHeaders.parse(response);
+        await _headersCache.saveHeaders(requestUri, headers);
+
+        final html = response.data as String;
+
+        return RemoteResponse.withNewData(html, maxPage: 0);
+      } else {
+        /// This else check is never run but just incase,
+        /// something went wrong with the server
+        throw RestApiException(response.statusCode);
+      }
+    } on DioError catch (e) {
+      if (e.isNoConnectionError) {
+        return const RemoteResponse.noConnection();
+      } else if (e.response != null) {
+        throw RestApiException(e.response?.statusCode);
+      } else {
+        rethrow;
+      }
+    }
+  }
+}
