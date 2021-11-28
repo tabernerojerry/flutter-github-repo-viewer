1bd3101ee93d501bbf514887d8774d42d1945823
Jerry Tabernero <jerry.tabernero@opcbiz.com>
2021-11-28 15:48:58 +0800

    repo detail provider

--- lib/github/core/shared/providers.dart
+++ lib/github/core/shared/providers.dart
@@ -1,6 +1,10 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:repo_viewer/core/shared/providers.dart';
 import 'package:repo_viewer/github/core/infrastructure/github_headers_cache.dart';
+import 'package:repo_viewer/github/detail/application/repo_detail_notifier.dart';
+import 'package:repo_viewer/github/detail/infrastructure/repo_detail_local_service.dart';
+import 'package:repo_viewer/github/detail/infrastructure/repo_detail_remote_service.dart';
+import 'package:repo_viewer/github/detail/infrastructure/repo_detail_repository.dart';
 import 'package:repo_viewer/github/repos/core/application/paginated_repos_notifier.dart';
 import 'package:repo_viewer/github/repos/searched_repos/application/searched_repos_notifier.dart';
 import 'package:repo_viewer/github/repos/searched_repos/infrastructure/searched_repos_remote_service.dart';
@@ -56,5 +60,33 @@ final searchedReposNotifierProvider = StateNotifierProvider.autoDispose<
     SearchedReposNotifier, PaginatedReposState>(
   (ref) => SearchedReposNotifier(
     ref.watch(searchedReposRepository),
+  ),
+);
+
+final repoDetailLocalServiceProvider = Provider(
+  (ref) => RepoDetailLocalService(
+    ref.watch(sembastProvider),
+    ref.watch(githubHeadersCacheProvider),
+  ),
+);
+
+final repoDetailRemoteServiceProvider = Provider(
+  (ref) => RepoDetailRemoteService(
+    ref.watch(dioProvider),
+    ref.watch(githubHeadersCacheProvider),
+  ),
+);
+
+final repoDetailRepositoryProvider = Provider(
+  (ref) => RepoDetailRepository(
+    ref.watch(repoDetailLocalServiceProvider),
+    ref.watch(repoDetailRemoteServiceProvider),
+  ),
+);
+
+final repoDetailNotifierProvider =
+    StateNotifierProvider.autoDispose<RepoDetailNotifier, RepoDetailState>(
+  (ref) => RepoDetailNotifier(
+    ref.watch(repoDetailRepositoryProvider),
   ),
 );
